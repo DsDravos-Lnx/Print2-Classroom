40e185556133592576307d58337a489133b20385
DsDravos-lnx <ds.hallucinate@gmail.com>
2021-03-18 18:05:12 -0300

    commit classroomstable

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const UserController = require('./controllers/UserController')
+const ClassroomController = require('./controllers/ClassroomController')
 
 const routes = express.Router()
 
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.create)
+
+routes.get('/classrooms', ClassroomController.index)
+routes.post('/classrooms', ClassroomController.create)
+
+
 
 module.exports = routes
